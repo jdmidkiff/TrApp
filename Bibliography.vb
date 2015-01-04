e2414ba057eb5b2a6a75a2fd6466c0a4b1657be7
MonSoon1900 <midkiff.km@gmail.com>
2015-01-03 20:45:09 -0800

    Revert "Initial Commit"
    This reverts commit 867146c51e767e7fdd7700355916103deac68046.

--- Bibliography.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Bibliography
-
-    Private Sub lblsources_Click(sender As System.Object, e As System.EventArgs) Handles lblsources.Click
-
-    End Sub
-End Class
--- Storage.vb
+++
@@ -1,6 +0,0 @@
-﻿Module Storage
-    Public Username As String
-    Public TotalScore As Integer
-    Public Scoreboard As New Collection
-    ''System.IO.Streamwriter("Scoreboard.txt", True)
-End Module
